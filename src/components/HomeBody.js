c3992e4b8ca5523be29233703e90b2488fd0be43
badtype <g.c.faina@gmail.com>
2023-09-13 11:56:56 +0200

    styled system: Home Page

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -1,12 +1,59 @@
 import React, { useState, useEffect } from "react";
-// import { Box, Button, Container, InputWrapper, Stack } from "./styled";
+import { Box, Button, Container, InputWrapper, Stack } from "./styled";
 import { ReactComponent as SearchIcon } from "../images/search-media.svg";
 import PhotoSection from "./Photo-Section";
 import Paginator from "./Paginator";
 
 
 const HomeBody = () => {
-  return <>Home Body</>;
+  return (
+    <Container security="fullwidth" mt={["24px", "72px"]}>
+      <Container mt="96px">
+        <Stack justify="space-between" align="end">
+          <h4>search your photos</h4>
+          <p
+            style={{
+              color: "var(grey-700)",
+            }}
+          >
+          </p>
+        </Stack>
+
+        <Box mt="24px">
+          <Stack
+            width="fit-content"
+            bg="grey.900"
+            borderRadius="100px"
+            border="1px solid"
+            px="18px"
+            style={{
+              overFlowX: "hidden"
+            }}
+          >
+            <InputWrapper
+              placeholder="cerca foto"
+              border="none"
+              pl="0px"
+              value="React"
+              onChange={() => {
+                return;
+              }}
+            />
+
+            <Button
+              rightIcon={<SearchIcon />}
+              isLoading={false}
+              disabled={false}
+              variant="text"
+              iconColor="grey.700"
+              bg="grey.900"
+            />
+
+          </Stack>
+        </Box>
+      </Container>
+    </Container>
+  );
 };
 
 export default HomeBody; 
--- src/screen/home-screen.js
+++ src/screen/home-screen.js
@@ -1,68 +1,17 @@
 import React from "react";
 import Layout from "../components/layout";
 import { Box, Container, Stack, InputWrapper, Skeleton, Button } from "../components/styled";
+import Hero from "../components/Hero";
+import HomeBody from "../components/HomeBody";
 
 
 
 const HomePage = () => {
   return (
     <Layout>
-      <Container size="xl" m="48px">
-        <Stack ml="50px">
-          <h1>Home</h1>
-        </Stack>
-        <Stack spacing="50px" ml="50px" /**direction="column"*/>
-
-          <Box
-            width="100px"
-            height="100px"
-            border="1px solid"
-            borderColor="red"
-            borderRadius="16px"
-            bg="green"
-          ></Box>
-          <Box
-            width="100px"
-            height="100px"
-            border="1px solid"
-            borderColor="red"
-            borderRadius="16px"
-            bg="green"
-          ></Box>
-          <Box
-            width="100px"
-            height="100px"
-            border="1px solid"
-            borderColor="red"
-            borderRadius="16px"
-            bg="green"
-          ></Box>
-          <Box
-            width="100px"
-            height="100px"
-            border="1px solid"
-            borderColor="red"
-            borderRadius="16px"
-            bg="green"
-          ></Box>
-        </Stack>
-
-        <Box my="96px" mx="50px">
-          <InputWrapper placeholder="text" />
-        </Box>
-
-        <Skeleton
-          width="300px"
-          height="300px"
-          borderRadius="16px"
-          variant="circle"
-        />
-        <Stack spacing="24px" mt="96px" align="center">
-          <Button variant={"contained"} size="xl">Contained</Button>
-          <Button variant={"outlined"} size="md">Outlined</Button>
-          <Button variant={"text"} size="sm">Text</Button>
-          <Button variant={"disabled"} size="xl" >Disabled</Button>
-        </Stack>
+      <Container size="fullwidth" >
+        <Hero />
+        <HomeBody />
       </Container>
     </Layout >
   );
